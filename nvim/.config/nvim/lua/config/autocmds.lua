dd88c507bd1b39ec7eab0803a50be08b538e1283
khalidmesbah <swekhalidmesbah6@gmail.com>
2024-09-14 03:45:21 +0300

    update

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -32,18 +32,8 @@ vim.cmd [[
 -- Define an autocommand group
 vim.api.nvim_create_augroup('FileChange', { clear = true })
 
--- Run a script whenever an mdx file is written (saved)
+-- Update the last modified date of the mdx file
 vim.api.nvim_create_autocmd('BufWritePost', {
-  group = 'FileChange',
   pattern = '*.mdx',
-  callback = function()
-    -- Get the full path of the current file
-    local file_path = vim.fn.expand '%:p'
-
-    -- Construct the command to run the script with the file path as an argument
-    local command = string.format('silent !python3 /home/khalidmesbah/repos/workflow/scripts/update-date.py "%s"', file_path)
-
-    -- Execute the command
-    vim.api.nvim_command(command)
-  end,
+  command = 'silent !python ~/repos/workflow/scripts/update-date.py %',
 })
